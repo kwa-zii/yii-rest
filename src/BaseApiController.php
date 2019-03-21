b6a6a66ed0741893252301007b7054e35f1a0fba
秦启威 <qinqiwei@hotmail.com>
2019-03-21 16:14:19 +0800

    增加跨域请求方法、请求头、域设置

--- src/BaseApiController.php
+++ src/BaseApiController.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * A lightweight and powerful Restful interface framework based on Yii2
- *
+ * PHP version 5.6 or newer
+ * 
  * @category Restful_Api_Framework
  * @package  Qinqw\Yii\Rest
  * @author   Kevin <qinqiwei@hotmail.com>
@@ -37,6 +38,55 @@ class BaseApiController extends ActiveController
             'collectionEnvelope' => 'data' 
     ];
     public $modelClass = "";
+    //Access-Control-Allow-Origin:*
+    public $arr_acao = [
+        '*'
+    ];
+    //Access-Control-Allow-Methods
+    public $arr_acam = [
+        'POST', 
+        'PUT', 
+        'GET', 
+        'DELETE',
+        'OPTIONS'
+    ];
+    //Access-Control-Allow-Headers
+    public $arr_acah = [
+        'token', 
+        'app-key',
+        'content-type',
+    ];
+
+    /**
+     * __construct
+     * 
+     * @param mixed $id 
+     * @param mixed $module 
+     * 
+     * @return mixed 
+     */
+    public function __construct($id, $module)
+    {
+        parent::__construct($id, $module);
+        if (isset(Yii::$app->params['access_control'])) {
+            $access_control = Yii::$app->params['access_control'];
+            if (isset($access_control['allow_origin'])
+                && is_array($access_control['allow_origin'])
+            ) {
+                $this->arr_acao = $access_control['allow_origin'];
+            }
+            if (isset($access_control['allow_methods'])
+                && is_array($access_control['allow_methods'])
+            ) {
+                $this->arr_acam = $access_control['allow_methods'];
+            }
+            if (isset($access_control['allow_headers'])
+                && is_array($access_control['allow_headers'])
+            ) {
+                $this->arr_acah = $access_control['allow_headers'];
+            }
+        }
+    }
 
     /**
      * Behaviors
@@ -81,22 +131,18 @@ class BaseApiController extends ActiveController
     {
         defined('YII_DEBUG') or define('YII_DEBUG', true);
         $req = Yii::$app->request;
+        header('Access-Control-Allow-Origin: '.implode(',', $this->arr_acao));
+        header('Access-Control-Allow-Methods: '.implode(',', $this->arr_acam));
+        header('Access-Control-Allow-Headers: '.implode(',', $this->arr_acah));
+        header("Access-Control-Max-Age: 86400");
+
         if ($req->isOptions) {
-            //允许跨域请求处理
-            header("Access-Control-Allow-Origin:*");
-            header("Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS");
-            header("Access-Control-Allow-Headers: token, app-key, content-type, etcp-base");
-            header("Access-Control-Max-Age: 86400");
-            
             $code = "202";
             $message = "Accepted";
             header("HTTP/1.1 ".$code." ".$message); 
             exit();
         }
-        header("Access-Control-Allow-Origin:*");
-        header("Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS");
-        header("Access-Control-Allow-Headers: token, app-key, content-type, etcp-base");
-        header("Access-Control-Max-Age: 86400");
+        
         /** 
          * 此部分代码用户修复 Rest控制器报 Response content must not be an array. 的错误
          *
